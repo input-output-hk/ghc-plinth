088274ab6265753e73778cb7ccefedfa55e1f2f7
Luite Stegeman <stegeman@gmail.com>
2026-02-15 06:58:05 +0100

    add Project Unit Id
    hopefully this will help Cabal keep our custom GHC packages separate
    full support will be available in GHC 9.8+

--- compiler/GHC/Driver/Session.hs
+++ compiler/GHC/Driver/Session.hs
@@ -4708,6 +4708,7 @@ compilerInfo dflags
        ("Project Patch Level",         cProjectPatchLevel),
        ("Project Patch Level1",        cProjectPatchLevel1),
        ("Project Patch Level2",        cProjectPatchLevel2),
+       ("Project Unit Id",             "ghc-" ++ projectVersion dflags ++ "-plinth"),
        ("Booter version",              cBooterVersion),
        ("Stage",                       cStage),
        ("Build platform",              cBuildPlatformString),
