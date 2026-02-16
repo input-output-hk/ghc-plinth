9561dc0faab58a7fb3c740e4dd64fdabfbc1117a
Luite Stegeman <stegeman@gmail.com>
2026-02-16 14:14:06 +0100

    some untested changes for build script

--- plinth-build.sh
+++ plinth-build.sh
@@ -35,7 +35,7 @@ if ! version_ge "$CABAL_VERSION" "3.14.2.0"; then
 fi
 if [ -z "$HAPPY" ] || [ ! -x "$HAPPY" ]; then
   echo "Happy not found, building happy-1.20.1.1 locally..."
-  "$CABAL" install happy-1.20.1.1 -w "$GHC" --install-method=copy --installdir="$PWD/_build/tools"
+  "$CABAL" install happy-1.20.1.1 -w "$GHC" --install-method=copy --overwrite-policy=always --installdir="$PWD/_build/tools"
   HAPPY="$LOCAL_HAPPY"
   if [ ! -x "$HAPPY" ]; then
     echo "Failed to build happy"
@@ -77,8 +77,8 @@ fi
 
 STAGE="stage-plinth"
 BOOT_GHC="$BASE/_build/stage1/bin/ghc"
-TARGET_PLATFORM=`"$BOOT_GHC" --print-target-platform`
-VERSION=`"$BOOT_GHC" --numeric-version`
+TARGET_PLATFORM=$("$BOOT_GHC" --print-target-platform)
+VERSION=$("$BOOT_GHC" --numeric-version)
 
 CABAL_PROJECT_ARGS="--project-file=cabal.project"
 
@@ -99,26 +99,49 @@ CABAL_BUILD_ARGS="\
 )
 
 # add uplc-ghc to the _build dir and the bindists
-DEST_UPLC_GHC="$BASE/_build/stage1/bin/uplc-ghc"
+CWRAPPER_DIR="$BASE/hadrian/bindist/cwrappers"
+
+# detect Windows by checking the target platform
+case "$TARGET_PLATFORM" in
+    *-mingw32|*-windows) IS_WINDOWS=1; EXE_EXT=".exe" ;;
+    *)                   IS_WINDOWS=0; EXE_EXT="" ;;
+esac
+
+DEST_UPLC_GHC="$BASE/_build/stage1/bin/uplc-ghc${EXE_EXT}"
+
 (
     cd plinth
-    SRC_UPLC_GHC=`cabal ${CABAL_PROJECT_ARGS} ${CABAL_ARGS} list-bin ${CABAL_BUILD_ARGS} ghc:ghc`
+    SRC_UPLC_GHC=$("$CABAL" ${CABAL_PROJECT_ARGS} ${CABAL_ARGS} list-bin ${CABAL_BUILD_ARGS} ghc:ghc)
     echo "installing uplc-ghc: $SRC_UPLC_GHC => $DEST_UPLC_GHC"
 
     # add to build dir
     cp "$SRC_UPLC_GHC" "$DEST_UPLC_GHC"
 
     # add to bindist
-    for dir in $BASE/_build/bindist/*; do
+    for dir in "$BASE"/_build/bindist/*; do
         if [ -d "$dir" ]; then
             echo "adding uplc-ghc to bindist: $dir"
-            cp "$SRC_UPLC_GHC" "$dir/bin/uplc-ghc-$VERSION"
-            ( cd "$dir/bin" && ln -sf "uplc-ghc-$VERSION" "uplc-ghc" )
+            if [ "$IS_WINDOWS" -eq 1 ]; then
+                # Windows: copy versioned .exe and compile a C wrapper
+                cp "$SRC_UPLC_GHC" "$dir/bin/uplc-ghc-$VERSION.exe"
+                "$BOOT_GHC" -no-hs-main \
+                    -o "$dir/bin/uplc-ghc.exe" \
+                    "-I$CWRAPPER_DIR" \
+                    "-DEXE_PATH=\"uplc-ghc-$VERSION.exe\"" \
+                    "-DINTERACTIVE_PROCESS=0" \
+                    "$CWRAPPER_DIR/version-wrapper.c" \
+                    "$CWRAPPER_DIR/getLocation.c" \
+                    "$CWRAPPER_DIR/cwrapper.c"
+            else
+                # Unix: copy versioned binary and symlink
+                cp "$SRC_UPLC_GHC" "$dir/bin/uplc-ghc-$VERSION"
+                ( cd "$dir/bin" && ln -sf "uplc-ghc-$VERSION" "uplc-ghc" )
+            fi
         fi
     done
 )
 
 # rebuild archives to include uplc-ghc
-rm -f $BASE/_build/bindist/ghc-*.tar.*
+rm -f "$BASE"/_build/bindist/ghc-*.tar.*
 ./hadrian/build -j --flavour=release binary-dist
 
